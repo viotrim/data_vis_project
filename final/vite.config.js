2741a1dd8b49e751f3868124433c31e18d5cc54c
viotrim <2000010632@stu.pku.edu.cn>
2025-01-05 02:12:00 +0000

    push

--- final/vite.config.js
+++ final/vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === "production" ? "/data_vis_project/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
